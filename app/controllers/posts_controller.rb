fdee9f304f650a5efc6c54a4d12bf44bdf6a79f8
Ryan Brunner <ryan@influitive.com>
2016-02-18 23:02:30 +0000

    Week 4, Day 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,17 +8,40 @@ class PostsController < ApplicationController
   end
   
   def create
-    Post.create({
-      title: params[:post][:title],
-      content: params[:post][:content],
-      author: params[:post][:author],
-      category: params[:post][:category]
-    })
-    redirect_to root_path
+    if post_params[:title].blank?
+      render text: 'You need to provide a title'
+    elsif post_params[:content].blank?
+      render text: 'You need to provide content'
+    else
+      Post.create(post_params)
+      redirect_to root_path
+    end
+  end
+  
+  # /posts/:id -> params[:id] 
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    if post_params[:title].blank?
+      render text: 'You need to provide a title'
+    elsif post_params[:content].blank?
+      render text: 'You need to provide content'
+    else
+      @post = Post.find(params[:id])
+      @post.update(post_params)
+    
+      redirect_to root_path
+    end
   end
   
   def new
     @post = Post.new
+  end
+  
+  def post_params
+    params.require(:post).permit(:title, :author, :content, :category)
   end
   
   def 💩
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,28 @@
 class Post < ActiveRecord::Base
+    # Validate presence - invalid if title is nil or empty
+    validates :title, presence: true
+    validates :category, inclusion: { in: ['news', 'fun']}, if: :trudy?
+    validates :category, inclusion: { in: ['news', 'fun', 'announcements']}, if: :ryan?
+    
+    def trudy?
+      self.author == 'trudy'
+    end
+    
+    def ryan?
+      self.author == 'ryan'
+    end
+    
+    
+    # validates :category, format: //
+    
+    # Validate that this is a number
+    # validates :category, numericality: true
+    
+    # Validate that this value is in a list
+    validates :category, inclusion: { in: ['news', 'fun'] }
+    
     def lead
-        content.first(100) + "..."
+        content.to_s.first(100) + "..."
     end
     
     def to_s
