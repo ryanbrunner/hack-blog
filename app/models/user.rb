b6f94bee4918d2649cee2147e64eaf1715ed9c35
Ryan Brunner <ryan@influitive.com>
2016-02-19 02:17:24 +0000

    Users stuff

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Base
+    validates :email, presence: { allow_nil: true }, 
+                      uniqueness: { allow_nil: true }
+                      
+    validate :at_least_18
+    validate :email_domain
+    
+    def email_domain
+      if not email.ends_with?("ryanbrunner.ca")
+        errors.add(:email, "must be from ryanbrunner.ca")          
+      end
+    end
+    
+    def at_least_18
+      if date_of_birth > 18.years.ago
+        errors.add(:date_of_birth, "must be at least 18") 
+      end
+    end
+end
